e1025c6878662228483ef4feb8681a31cd369107
Swarup Vishwas <swarupvishwas000@gmail.com>
2023-08-31 22:15:39 +0530

    codeindex and comment

--- flask/main.py
+++ flask/main.py
@@ -23,5 +23,39 @@ def main():
     return render_template("index.html")
 
 
+@app.route("/comment", methods=["POST", "GET"])
+def comment():
+    if request.method == "POST":
+        f = request.files["js-file"]
+        f.save(f.filename)
+        js_path = f.filename
+        with open(js_path) as f:
+            js_code = f.read()
+        python_code = js_to_python(js_code)
+        js = js_code.split("\n")
+        python_code = indentToNbsp(python_code)
+        py = python_code.split("\n")
+        print(py)
+        return render_template("comment.html", js=js, py=py)
+    return render_template("comment.html")
+
+
+@app.route("/codeindex", methods=["POST", "GET"])
+def codeindex():
+    if request.method == "POST":
+        f = request.files["js-file"]
+        f.save(f.filename)
+        js_path = f.filename
+        with open(js_path) as f:
+            js_code = f.read()
+        python_code = js_to_python(js_code)
+        js = js_code.split("\n")
+        python_code = indentToNbsp(python_code)
+        py = python_code.split("\n")
+        print(py)
+        return render_template("codeindex.html", js=js, py=py)
+    return render_template("codeindex.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
